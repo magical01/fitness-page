4e345ce7be5d43b3024a61f048895a42032ed9ac
magical01 <denis.verzakov.015@gmail.com>
2022-09-12 23:09:49 +0500

    feat: add footer

--- src/js/components/trainers-slider.js
+++ src/js/components/trainers-slider.js
@@ -2,7 +2,7 @@ import Swiper, { Navigation, Pagination } from 'swiper';
 Swiper.use([Navigation, Pagination]);
 const swiper = new Swiper('.trainers__slider', {
   loop: true,
-  slidesPerView: '1',
+  slidesPerView: '3',
   // spaceBetween: 60,
   navigation: {
     nextEl: '.trainers-btn-next',
