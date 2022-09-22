68c30021070d7b1091fec1b69f8dace26f58508c
magical01 <denis.verzakov.015@gmail.com>
2022-09-22 22:19:22 +0500

    feat: add adaptive

--- src/js/components/marathon-circle.js
+++ src/js/components/marathon-circle.js
@@ -5,23 +5,23 @@ document.addEventListener('DOMContentLoaded', () => {
   circle.forEach((elem, i) => {
     elem?.addEventListener('mouseover', () => {
       if (document.querySelector('.marathon__number--active')) {
-        document.querySelector('.marathon__number--active').classList.remove('marathon__number--active')
+        document.querySelector('.marathon__number--active').classList.remove('marathon__number--active');
       }
 
       if (document.querySelector('.marathon__text--active')) {
-        document.querySelector('.marathon__text--active').classList.remove('marathon__text--active')
+        document.querySelector('.marathon__text--active').classList.remove('marathon__text--active');
       }
 
       elem.classList.add('marathon__number--active');
       text[i].classList.add('marathon__text--active');
-    })
+    });
   });
 
   circle.forEach((elem, i) => {
     elem?.addEventListener('mouseleave', () => {
       elem.classList.remove('marathon__number--active');
       text[i].classList.remove('marathon__text--active');
-    })
+    });
   });
 
-})
+});
--- src/js/components/round-letters.js
+++ src/js/components/round-letters.js
@@ -1,16 +1,16 @@
-// function initRoundedLetters(mainSelector, count, selector) {
-//   let $container = document?.querySelector(`.${mainSelector}`);
-//   let letters = String($container.textContent.trim() + "   ").repeat(count).split("");
-//   let output = "";
-//   const minAngle = 360 / letters.length;
-//   letters.forEach((letter, id) => {
-//     let angle = Math.trunc(minAngle * id);
-//     output += `<span class="${selector}" style="transform: rotate(${angle}deg);">${letter}</span>`;
-//   });
-//   $container.innerHTML = output;
-// }
+function initRoundedLetters(mainSelector, count, selector) {
+  let $container = document?.querySelector(`.${mainSelector}`);
+  let letters = String($container.textContent.trim() + "   ").repeat(count).split("");
+  let output = "";
+  const minAngle = 360 / letters.length;
+  letters.forEach((letter, id) => {
+    let angle = Math.trunc(minAngle * id);
+    output += `<span class="${selector}" style="transform: rotate(${angle}deg);">${letter}</span>`;
+  });
+  $container.innerHTML = output;
+}
 
-// initRoundedLetters('round__text', 3, 'hero__letter');
+initRoundedLetters('round__text', 3, 'hero__letter');
 // initRoundedLetters('free__letters', 2, 'free__letter');
 // initRoundedLetters('trial__letters', 2, 'trial__letter');
 
