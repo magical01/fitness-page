67458313f32693702e9d7987738f33cd94b88fc8
magical01 <denis.verzakov.015@gmail.com>
2022-10-16 14:49:41 +0500

    fix: w3c validator

--- src/js/components/accordion.js
+++ src/js/components/accordion.js
@@ -4,13 +4,20 @@ document.addEventListener('DOMContentLoaded', () => {
   accordion.forEach((elem) => {
     elem?.addEventListener('click', (e) => {
       const self = e.currentTarget;
-      const dropdown = self?.querySelector('.dropdown')
+      const top = self?.querySelector('.accordion__top');
+      const dropdown = self?.querySelector('.dropdown');
 
       self.classList.toggle('accordion__item--active');
 
       if (self.classList.contains('accordion__item--active')) {
+        top.setAttribute('aria-label', 'Закрыть ответ');
+        top.setAttribute('aria-expanded', true);
+				dropdown.setAttribute('aria-hidden', false);
         dropdown.style.maxHeight = dropdown.scrollHeight + 'px';
       } else {
+        top.setAttribute('aria-label', 'Открыть ответ');
+        top.setAttribute('aria-expanded', false);
+				dropdown.setAttribute('aria-hidden', true);
         dropdown.style.maxHeight = null;
       }
     })
